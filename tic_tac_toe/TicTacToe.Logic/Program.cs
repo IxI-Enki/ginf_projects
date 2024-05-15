50dd459b8616f9f985f00bc7e38456841d91330b
IxI-Enki <cell.enohp.apps@gmail.com>
2024-05-16 01:43:25 +0200

    Initialize Tic Tac Toe

---
+++ tic_tac_toe/TicTacToe.Logic/Program.cs
@@ -0,0 +1,71 @@
+﻿#region header ( class: TicTacToe - general information )
+// - This region stores info about the author and the program( version, features,.. )
+/*
+ *    ╔══════════════════════╦═══════════════════════════════════════════════════════════════════╗
+ *    ║   HTBLA - Leonding   ║ ┏━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ ║█
+ *    ╠═════════╤════════════╣ ┃▷ DESCRIPTION: │                                               ┃ ║█
+ *    ║  Name   │  Jan Ritt  ║ ┃               │                                               ┃ ║█
+ *    ╟─────────┼────────────╢ ┃    GINF01     │                                               ┃ ║█
+ *    ║  Class  │  4 ACIFT   ║ ┠───────────────┼───────────────────────────────────────────────┨ ║█
+ *    ╟─────────┼────────────╢ ┃▷ SYNTAX:      │ TicTacToe                                     ┃ ║█
+ *    ║  Year   │  2024      ║ ┃ ► _nameHere_  │                                               ┃ ║█
+ *    ╟─────────┴────────────╢ ┃               │                                               ┃ ║█
+ *    ║  Last edit:  5.2024  ║ ┃  ╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ┃ ║█
+ *    ╚══════════════════════╣ ┃▷ VERSION:     │ 1.1                                           ┃ ║█
+ *                           ║ ┗━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ ║█
+ *    ┏━━━━━━━━━━━━━━━━━┓    ╚═══════════════════════════════════════════════════════════════════╝█
+ *    ┃   Development   ┃      ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+ *    ┣━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ *    ┃ ▷ KNOWN ISSUES  │                                                                        ┃█
+ *    ┠─────────────────┼────────────────────────────────────────────────────────────────────────┨█
+ *    ┃ ▷ NEXT UP       │                                                                        ┃█
+ *    ┃  ╌╌╌╌╌╌╌╌╌╌╌╌╌  │ ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌  ┃█
+ *    ┃    ► _here_     │                                                                        ┃█
+ *    ┗━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛█
+ *      ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+*/
+#endregion
+
+#region using
+// - imported Libraries
+
+#endregion
+
+namespace Projects
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.Write("\n " + Header);
+
+    }
+
+    static string Header
+      =>
+        ColoredString(" Tic", "40;40;255")
+      + ColoredString(" Tac", "255;40;40")
+      + ColoredString(" Toe", "40;255;40");
+
+    // (☞ﾟヮﾟ)☞   Easy setup for colored strings   ☜(ﾟヮﾟ☜)
+    static string AnsiEsc = "\u001b[",
+                  Foreground = "3",
+                  Background = "4",
+                  Modifier = "8;2;",
+                  Reset = AnsiEsc + "0m";
+    static byte Red = 255,
+                Green = 255,
+                Blue = 255;
+    static byte[] RGBValues = [Red, Green, Blue];
+    static string RGBString => string.Join(';', RGBValues);
+
+    static string ColoredString(string toColor, string rgbString = "255;255;255", bool foreground = true)
+          => AnsiEsc
+          + (foreground ? Foreground : Background)
+          + Modifier
+          + (rgbString == RGBString ? RGBString : rgbString)
+          + 'm'
+          + toColor
+          + Reset;
+  }
+}
