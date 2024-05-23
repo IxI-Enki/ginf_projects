94694747dc507a0fbdd70078dc285ea8c6cb7ed6
IxI-Enki <cell.enohp.apps@gmail.com>
2024-05-23 14:10:29 +0200

    enabled multipe outputs

--- regex_helper/RegexHelper.Logic/Program.cs
+++ regex_helper/RegexHelper.Logic/Program.cs
@@ -5,7 +5,6 @@ using System.Reflection;
 
 namespace RegexLogic
 {
-
   class Program
   {
     static string exampleString => File.ReadAllText(Environment.CurrentDirectory + "\\TestText.txt");
@@ -31,12 +30,14 @@ namespace RegexLogic
                           "\n" +
                           "\n");
         Console.WriteLine(input == "0" ? "Der Beispiel Text enthält eine Telefonnummer,\n  mit dem pattern: \"4 Dezimalzahlen / 8 Dezimalzahlen\" " +
-                                      "\n" + @"-> verwenden Sie zum finden das Regex-Pattern: \d{4}/\d{8} " : "") ;
+                                      "\n" + @"-> verwenden Sie zum finden das Regex-Pattern: \d{4}/\d{8} " : "");
         Console.ResetColor();
 
         if (input == "0")
+        {
           input = exampleString;
-
+          Console.Write("\n" + input + "\n\n\n");
+        }
         Console.WriteLine(" Geben Sie das zu suchende pattern ein:\n ");
         string pattern = Console.ReadLine();
 
@@ -54,7 +55,6 @@ namespace RegexLogic
         Regex regexNormal = new Regex(@"\((?<inner>[^\)]+)\)");
         Regex regexCurly = new Regex(@"\{(?<inner>[^\}]+)\}");
         Regex regexSquare = new Regex(@"\[(?<inner>[^\]]+)\]");
-
 
         // Muster in Text suchen
         Match match = regex.Match(input);
@@ -83,66 +83,60 @@ namespace RegexLogic
           Console.ForegroundColor = ConsoleColor.Green;
           Console.Write($" wurde {count} mal gefunden: ");
 
+          foreach (Match m in regex.Matches(input))
+          {
+            Console.Write("\n  \"" + m.Value + "\"");
+
+          }
+
           Console.WriteLine("\n Wenn Sie code durchsuchen, \n könnte es zum Beispiel helfen Klammern aufzulösen..");
           Console.WriteLine("Klammern auflösen? [j/n]");
 
           bool showBrackets = Console.ReadKey().KeyChar.ToString().ToUpper() == "J";
           foreach (string word in words)
-          {
-
-            if (pattern == @"(?<pair>\((?<inner>[^\)]+)\))")
+          { 
+            if (showBrackets)
             {
-              Console.ForegroundColor = ConsoleColor.DarkYellow;
-              string innerText = match.Groups["inner"].Value;
-              Console.Write("\n Gefundene Klammern:\n " + match.Value);
-              Console.ForegroundColor = ConsoleColor.Yellow;
-              Console.Write("\n Inhalt der Klammer:\n " + innerText + "\n");
-            }
-            else
-            {
-              if (showBrackets)
+              // Durchläuft alle Treffer für normale Klammern
+              foreach (Match mk in regexNormal.Matches(input))
               {
-                // Durchläuft alle Treffer für normale Klammern
-                foreach (Match mk in regexNormal.Matches(input))
-                {
-                  // Ausgabe der gefundenen Klammern und des Inhalts
-                  string brackets = mk.Value;
-                  string innerText = mk.Groups["inner"].Value;
+                // Ausgabe der gefundenen Klammern und des Inhalts
+                string brackets = mk.Value;
+                string innerText = mk.Groups["inner"].Value;
 
-                  Console.ForegroundColor = ConsoleColor.DarkYellow;
-                  Console.WriteLine("\n Gefundene Klammern (normal):\n " + brackets);
-                  Console.ForegroundColor = ConsoleColor.Yellow;
-                  Console.WriteLine("\n Inhalt der Klammer:\n " + innerText + "\n");
-                }
-
-                // Durchläuft alle Treffer für geschwungene Klammern
-                foreach (Match mk in regexCurly.Matches(input))
-                {
-                  // Ausgabe der gefundenen Klammern und des Inhalts
-                  string brackets = mk.Value;
-                  string innerText = mk.Groups["inner"].Value;
-
-                  Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                  Console.WriteLine("\n Gefundene Klammern (geschwungen):\n " + brackets);
-                  Console.ForegroundColor = ConsoleColor.Magenta;
-                  Console.WriteLine("\n Inhalt der Klammer:\n " + innerText + "\n");
-                }
-
-                // Durchläuft alle Treffer für eckige Klammern
-                foreach (Match mk in regexSquare.Matches(input))
-                {
-                  // Ausgabe der gefundenen Klammern und des Inhalts
-                  string brackets = mk.Value;
-                  string innerText = mk.Groups["inner"].Value;
-
-                  Console.ForegroundColor = ConsoleColor.DarkCyan;
-                  Console.WriteLine("\n Gefundene Klammern (eckig):\n " + brackets);
-                  Console.ForegroundColor = ConsoleColor.Cyan;
-                  Console.WriteLine("\n Inhalt der Klammer:\n " + innerText + "\n");
-                }
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("\n Gefundene Klammern (normal):\n " + brackets);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n Inhalt der Klammer:\n " + innerText + "\n");
               }
-              Console.WriteLine("  \"" + match.Value + "\"");
+
+              // Durchläuft alle Treffer für geschwungene Klammern
+              foreach (Match mk in regexCurly.Matches(input))
+              {
+                // Ausgabe der gefundenen Klammern und des Inhalts
+                string brackets = mk.Value;
+                string innerText = mk.Groups["inner"].Value;
+
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("\n Gefundene Klammern (geschwungen):\n " + brackets);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("\n Inhalt der Klammer:\n " + innerText + "\n");
+              }
+
+              // Durchläuft alle Treffer für eckige Klammern
+              foreach (Match mk in regexSquare.Matches(input))
+              {
+                // Ausgabe der gefundenen Klammern und des Inhalts
+                string brackets = mk.Value;
+                string innerText = mk.Groups["inner"].Value;
+
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("\n Gefundene Klammern (eckig):\n " + brackets);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\n Inhalt der Klammer:\n " + innerText + "\n");
+              }
             }
+          Console.WriteLine("  \"" + match.Value + "\"");
           }
         }
         else
@@ -157,9 +151,3 @@ namespace RegexLogic
     // --------------------------------------------------------------------------------------------
   }
 }
-
-
-#region notes
-// - place for notes and outdated code
-
-#endregion
